54bd1f0312a70510aa8b7aa2a3ad581207cef8be
alex <kadissa70@gmail.com>
2025-03-01 23:21:02 +0300

    create Dockerfile

--- store/models.py
+++ store/models.py
@@ -1,6 +1,5 @@
 """Django Stripe Orders"""
 
-
 from django.db import models
 
 
@@ -11,8 +10,6 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
-    def __repr__(self):
-        return classmethod.__class__
 
 
 class OrderItem(models.Model):
