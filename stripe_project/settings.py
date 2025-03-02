e70de403645564cadcb6fd800011c2daf5c45193
alex <kadissa70@gmail.com>
2025-03-02 13:29:11 +0300

    add migrations

--- stripe_project/settings.py
+++ stripe_project/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-DEBUG = os.getenv('DEBUG', 'False') == 'true'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 
 
